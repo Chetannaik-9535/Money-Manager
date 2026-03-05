9d7963ad83d093c09eb2f6815dd727dc43786b35
Chetannaik-9535 <naikc8468@gmail.com>
2026-03-05 17:35:56 +0000

    Solution

--- src/index.js
+++ src/index.js
@@ -12,5 +12,5 @@ ReactDOM.render(
 /*
   git config --global user.email naikc8468@gmail.com
     git config --global user.name Chetannaik-9535
-     git remote add origin https://github.com/Money-Manager.git
+     git remote add origin https://github.com/Chetannaik-9535/Money-Manager.git
 */
